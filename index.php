81defa81b4feaf8b5fe07b3534365f11386dc923
Blake Harley <blake@blakeharley.com>
2014-02-05 03:18:28 -0600

    Initial commit.

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Next Doge Coin Block Value</title>
+
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+		<link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.0/css/bootstrap.css" rel="stylesheet">
+		<link href="/css/doge.css" rel="stylesheet">
+
+		<!--[if lt IE 9]>
+			<script src="//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div class="container text-center">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3">
+					<img src="/img/doge.png" alt="Doge Coin Logo">
+					<h2>Next Block Value</h2>
+					<h1 id="next-value">Loading...</h1>
+
+					<hr>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-3 col-md-offset-3">
+					<b>Next Height</b><br>
+					<span id="next-height">&nbsp;</span>
+				</div>
+				<div class="col-md-3 hash-output">
+					<b>Current Hash</b><br>
+					<span id="previous-hash">&nbsp;</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 hash-output">
+					<hr>
+
+					<b>Much donate</b><br>
+					[Insert thinger]
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3">
+					<hr>
+
+					<b>A note on accuracy</b><br>
+					This calculator uses a slightly different version of the Mersenne
+					Twister random number generator than the one used in the real dogecoin
+					program. That being said, this page should be reasonably accuate.
+				</div>
+			</div>
+		</div>
+
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.0/js/bootstrap.min.js"></script>
+		<script>
+			$(function()
+			{
+				var nextValue = $('#next-value');
+				var nextHeight = $('#next-height');
+				var previousHash = $('#previous-hash');
+
+				var fetchData = function(data)
+				{
+					nextValue.html("&#x110;" + data.prettyValue);
+					nextHeight.html(data.height);
+					previousHash.html(data.hash);
+
+					setTimeout(function()
+					{
+						$.getJSON('/wow.php', fetchData);
+					}, 60000);
+				};
+
+				$.getJSON('/wow.php', fetchData);
+			});
+		</script>
+	</body>
+</html>
---
+++ wow.php
@@ -0,0 +1,84 @@
+<?php
+
+require 'mersenne_twister.php';
+use mersenne_twister\twister;
+
+/**
+ * Generates a random value using the Mersenne Twister method such that is more closely
+ * agrees with values produced by Boost's C++ libraries.
+ *
+ * @param  int $seed  The value to seed the random generator with
+ * @param  int $range The maximum value this random number can be
+ * @return int        The random number
+ */
+function generateRand($seed, $range)
+{
+	$twister = new twister($seed);
+	return $twister->rangereal_open(1, $range);
+}
+
+/**
+ * Gets the block value for the specified height and hash.
+ *
+ * @param  int    $height       The height to find
+ * @param  string $previousHash The hash of the previous block
+ * @return int                  The (approximate) value of the next block
+ */
+function getBlockValue($height, $previousHash)
+{
+	if ($height < 100000)
+	{
+		$seed = hexdec(substr($previousHash, 7, 7));
+		$rand = generateRand($seed, 999999);
+	}
+	else if ($height < 200000)
+	{
+		$seed = hexdec(substr($previousHash, 7, 7));
+		$rand = generateRand($seed, 499999);
+	}
+	else if ($height < 300000)
+	{
+		$seed = hexdec(substr($previousHash, 6, 7));
+		$rand = generateRand($seed, 249999);
+	}
+	else if ($height < 400000)
+	{
+		$seed = hexdec(substr($previousHash, 7, 7));
+		$rand = generateRand($seed, 124999);
+	}
+	else if ($height < 500000)
+	{
+		$seed = hexdec(substr($previousHash, 7, 7));
+		$rand = generateRand($seed, 62499);
+	}
+	else if ($height < 600000)
+	{
+		$seed = hexdec(substr($previousHash, 6, 7));
+		$rand = generateRand($seed, 31249);
+	}
+	else
+	{
+		$rand = 10000 - 1; // Offset the + 1 in the return
+	}
+
+	return 1 + $rand;
+}
+
+$payload = file_get_contents('http://dogechain.info/chain/Dogecoin/get_blocks');
+$payload = json_decode($payload, true);
+$block = $payload['aaData'][0];
+$height = $block[0] + 1;
+$hash = $block[8];
+$value = getBlockValue($height, $hash);
+
+$result = [
+	'height'      => $height,
+	'hash'        => $hash,
+	'value'       => $value,
+	'prettyValue' => number_format($value, 2),
+];
+
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-Type: application/json');
+echo json_encode($result);
